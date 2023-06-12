7174e00e104241674203dac72805173651ddb09a
ramya5544 <ramyavishvhin1995@gmail.com>
2023-06-12 14:12:29 +0530

    react app deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+// function App() {
+//   console.log("hello from component");
+
+//   return (
+//     <div><p>Hello world!</p></div>
+//   );
+// }
+
+// export default App;
+import React from 'react'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+   <h6>Free</h6>
+   <h1>$0</h1>/month
+   
+   <ul type="none">
+    <li>&#9989;Singe User</li>
+    <li>&#9989;5GB Storage</li>
+    <li>&#9989;Unlimited public projects</li>
+    <li>&#9989;community Access</li>
+    <li>&#x2716;Unlimited private Access</li>
+    <li>&#x2716;Dedicated phone support</li>
+    <li>&#x2716; Free subdomain</li>
+    <li>&#x2716;Monthly status reports</li>
+    <button type="button">Button</button>
+   </ul>
+   <h6>plus</h6>
+   <h1>$9</h1>/month
+   
+   <ul type="none">
+    <li>&#9989;5 Users</li>
+    <li>&#9989;50GB Storage</li>
+    <li>&#9989;Unlimited public projects</li>
+    <li>&#9989;community Access</li>
+    <li>&#9989;Unlimited private Access</li>
+    <li>&#9989;Dedicated phone support</li>
+    <li>&#9989;Free subdomain</li>
+    <li>&#x2716;Monthly status reports</li>
+    <button type="button">Button</button>
+   </ul>
+   <h6>Pro</h6>
+   <h1>$49</h1>/Month
+   <ul type="none">
+    <li>&#9989;Unlimited Users</li>
+    <li>&#9989;150GB Storage</li>
+    <li>&#9989;Unlimited public projects</li>
+    <li>&#9989;community Access</li>
+    <li>&#9989;Unlimited private Access</li>
+    <li>&#9989;Dedicated phone support</li>
+    <li>&#9989;Unlimited Free subdomain</li>
+    <li>&#9989;Monthly status reports</li>
+    <button type="button">Button</button>
+   </ul>
+   </div>
+  )
 }
 
-export default App;
+export default App
